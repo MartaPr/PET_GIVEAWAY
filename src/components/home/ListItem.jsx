b92d0375355bbe45f6dfd92689ada0762c83aa09
MartaPr <marta.praporscikaite@gmail.com>
2018-09-18 18:14:29 +0300

    img

--- src/components/home/ListItem.jsx
+++ src/components/home/ListItem.jsx
@@ -6,7 +6,7 @@ const ListItem = ({ item }) => {
     return (
         <div className="shelter_pet">
             <div className="polaroid_wrapper">
-                <img src={require(`../data/imagesdb/${item.img}`)} alt="oops" />
+                <img src={'./imagesdb/'+item.img} alt="oops" />
                 <div className="pet_nfo">
                     <div className="pet_name"> {item.name} </div>
                     <div className="pet_age"> {item.age} </div>
